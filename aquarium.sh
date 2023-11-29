c87eb255ea09ab98619482b3c84e088dd0cadb75
Lucie LONG <107046732+lucielong@users.noreply.github.com>
2023-11-29 12:13:51 +0100

    Update aquarium.sh

--- aquarium.sh
+++ aquarium.sh
@@ -2,4 +2,4 @@ dpkg --configure -a
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 usermod -aG docker $USER
-docker run -it danielkraic/asciiquarium
+docker run -it danielkraic/asciiquarium bash
