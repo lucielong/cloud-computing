320d4936bf619f86b0407a3e79574ce92b9b5e5e
Lucie LONG <107046732+lucielong@users.noreply.github.com>
2023-11-30 11:39:29 +0100

    Update and rename aquarium.sh to tetris.sh

--- tetris.sh
+++ tetris.sh
@@ -2,4 +2,4 @@ dpkg --configure -a
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 usermod -aG docker $USER
-docker run uzyexe/tetris
+docker run --name tetris --detach --publish 8888:80 ociotec/tetris
